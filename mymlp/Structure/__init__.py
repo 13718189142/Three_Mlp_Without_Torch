e34892f96b71d6abd582b9039b1269a9963f2f5f
Chen po <72637871+13718189142@users.noreply.github.com>
2025-04-02 11:32:08 +0800

    Delete mymlp directory

--- mymlp/Structure/__init__.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-from .activationfun import ReLU, LeakyReLu, tangenthyperbolic, sigmoid
-from .Layer import Dense
-from .model import *
--- mymlp/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def test(model, X_test, y_test):
-    probs = model.forward(X_test)
-    pred = np.argmax(probs, axis=1)
-    acc = np.mean(pred == y_test)
-    return acc
